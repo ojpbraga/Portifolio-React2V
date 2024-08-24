e248fa067f9b5c94b7d90d5352f643661c8f682e
João Pedro O. Braga <ojpbraga@gmail.com>
2024-08-24 02:28:59 -0300

    style: about me I

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import * as drei from '@react-three/drei'
 import * as fiber from '@react-three/fiber'
 import avatar from './images/avatar.mp4'
 import Particles from './components/Particles.tsx';
+import joaoPicture from './images/me.png'
+import { BorderBeam } from './components/BorderBeam.tsx'
 
 function App() {
   const { innerWidth: width, innerHeight: height } = window;
 
   return (
-    <>
+    <main className='grid gap-32 justify-center'>
       <Particles className='fixed z-[-1] h-[100dvh] w-[100%]'/>
 
       <section className="flex flex-col justify-between h-[105dvh] items-center">
@@ -38,10 +40,20 @@ function App() {
 
       </section>
 
-      <section className='h-screen '>
+      <section className='h-screen w-full flex flex-col items-center justify-center gap-2 xl:flex-row xl:w-[85vw] xl:gap-[5vw]'>
+
+        <div className=' relative w-[60vw] md:w-[40vw] xl:w-[25vw] bg-white/10 rounded-[30px]'>
+          <img className='rounded-[30px]' src={joaoPicture} alt=""/>
+          <BorderBeam colorTo='#d9d9d9' colorFrom='#fff'/>
+        </div>
+
+        <div className='w-[80vw]'>
+          <h1 className='text-[18vw] md:text-[14vw] xl:text-[8vw] xl:text-start font-bold text-center'>I'm João.</h1>
+          <p className='text-[9vw] md:text-[6vw] xl:text-[3vw] text-[#A1A1A1] font-bold'>A student of <span className='text-[#E1E1E1]'>software engineer</span>. I live in Belo Horizonte - MG <span>and...</span>👈</p>
+        </div>
         
       </section>
-    </>
+    </main>
   )
 }
 
