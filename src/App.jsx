aad31459c0dad651c18bb81deb86be3ed1d3a496
João Pedro O. Braga <ojpbraga@gmail.com>
2025-01-18 18:08:23 -0300

    style fix: button sticky

--- src/App.jsx
+++ src/App.jsx
@@ -74,10 +74,6 @@ function App() {
         profileDetails && 
           <div className='absolute z-10 w-[90vw] h-[90vh] bg-[#1B1B1B] rounded-[30px] flex flex-col p-5 gap-[2vh] overflow-scroll xl:overflow-hidden xl:flex-row xl:p-0'>
 
-            <div className="absolute w-12 h-12 bg-slate-600 right-5 rounded-[100px] grid place-items-center xl:top-5 cursor-pointer" onClick={() => setProfileDetails(state => !state)}>
-              <div className='h-2 w-7 rounded-full bg-white'></div>
-            </div>
-
             {/* Picture and data*/}
             <div className='flex flex-col xl:overflow-hidden gap-5 xl:w-[23vw] xl:m-5'>
               <div className='self-center relative w-[50vw] md:w-[40vw] xl:w-[15vw] xl:h-[35vh] bg-white/10 rounded-[30px] place-items-center overflow-hidden'>
@@ -90,14 +86,14 @@ function App() {
                 <h1 className='font-bold text-[6vw] xl:text-[1.6vw]'>João Pedro O. Braga</h1>
                 <div className='grid'>
                   <div className='grid grid-cols-2 gap-2'>
-                    <p className='text-sm text-[#B5B5B5] w-full flex justify-center items-center gap-1'><img className='h-4' src={location}/>
+                    <p className='text-sm text-[#B5B5B5] w-full flex items-center gap-1'><img className='h-4' src={location}/>
                     Belo Horizonte</p>
                     <p className='text-sm text-[#B5B5B5] '>22/12/2005</p>
                     <a href="/" className='text-sm text-[#B5B5B5] '>Github</a>
                     <a href="/" className='text-sm text-[#B5B5B5] '>Linkedln</a>
                   </div>
                 </div>
-                <p className='text-sm text-[#B5B5B5] '>Se for um site específico que foi lançado após recomendaria procurar mecanismo de busca ou em comunidades.</p>
+                <p className='text-sm text-[#B5B5B5] '>Se for um site específico que foi lançado após recomendaria procurar mecanismo de busca<br/>ou em comunidades.</p>
               </div>
             </div>
 
@@ -105,7 +101,7 @@ function App() {
             <div id='profile-div-scroll' className='gap-5 grid xl:overflow-y-scroll pt-5 w-full h-full'>
               <div>
                 <h1 className='text-[10vw] md:text-[7vw] xl:text-[4vw] xl:text-start font-bold'>I'm João.</h1>
-                <p className='text-[8.5vw] md:text-[5.5vw] xl:text-[2.5vw] text-[#A1A1A1] font-bold'>A student of <span className='text-[#E1E1E1]'>software engineer</span>. I live in <br/> Belo Horizonte - MG and I trying to <span className='text-[#E1E1E1]'>find a job</span> <br/> I live in Belo Horizonte - MG and. </p>
+                <p className='text-[7vw] md:text-[5.5vw] xl:text-[2.5vw] text-[#A1A1A1] font-bold'>A student of <span className='text-[#E1E1E1]'>software engineer</span>. I live in Belo Horizonte - MG and I trying to <span className='text-[#E1E1E1]'>find a job</span> I live in Belo Horizonte - MG and. </p>
               </div>
 
               <div>
@@ -120,6 +116,15 @@ function App() {
              
 
               
+            </div>
+
+            {/*  */}
+            <div className="sticky w-0 h-12  xl:top-6 right-20 cursor-pointer" onClick={() => { setProfileDetails(state => !state);
+            setScroll(state => !state);
+            }}>
+              <div className='bg-slate-600 rounded-[100px] grid place-items-center w-12 h-12'>
+                <div className='h-2 w-7 rounded-full bg-white'></div>
+              </div>
             </div>
 
           </div>
