980847c98fb9721bf07de57411e8a1b13030e378
João Pedro O. Braga <ojpbraga@gmail.com>
2025-01-20 00:45:04 -0300

    fix: canvas background I

--- src/App.jsx
+++ src/App.jsx
@@ -88,7 +88,35 @@ function App() {
             </ShaderGradientCanvas> */}
             <ShaderGradientCanvas style={{borderRadius: '30px'}}>
               <ShaderGradient
-              color1='#000055' color2='#0000BF' color3='#0000BF' animate='on' type='plane' cDistance={2} cAzimuthAngle={0} cPolarAngle={90} brightness={4} uDensity={0.5} positionX={4}></ShaderGradient>
+              color1='#000055' 
+              color2='#0000BF' 
+              color3='#0000BF' 
+              animate='on' 
+              type='sphere' 
+              cDistance={0.5} 
+              cAzimuthAngle={270} 
+              cPolarAngle={180} 
+              brightness={0.8} 
+              uDensity={0.8} 
+              uFrequency={5.5} 
+              uAmplitude={3.2} 
+              uStrength={0.3} 
+              uSpeed={0.3} 
+              positionX={0.5} 
+              positionY={0} 
+              positionZ={0} 
+              rotationX={0} 
+              rotationY={130} 
+              rotationZ={70} 
+              reflection={0.4} 
+              wireframe={false} 
+              cameraZoom={38} 
+              grain='on' 
+              lightType='env' 
+              envPreset='city' 
+              zoomOut={false}
+              hoverState='none'
+              ></ShaderGradient>
             </ShaderGradientCanvas>
             
           </div>
