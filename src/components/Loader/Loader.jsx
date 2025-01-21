0363a8b4f3d1c68604fbc4fa495cb9d2adc593cf
João Pedro O. Braga <ojpbraga@gmail.com>
2025-01-21 16:48:10 -0300

    style: Loader Responsive

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -11,12 +11,12 @@ const Loader = () => {
         <section className='grid place-items-center h-[100vh] w-full bg-black '>
             <div className='grid gap-7'>
         
-                <div className='grid place-items-center grid-cols-2 w-[10vw]'>
-                    <img src={airpod} alt="" className=' w-[5vw]' />
-                    <img src={airpod} alt="" className=' w-[5vw] scale-x-[-1]' />
+                <div className='grid place-items-center grid-cols-2 w-[35vw] xl:w-[10vw]'>
+                    <img src={airpod} alt=""/>
+                    <img src={airpod} alt="" className=' scale-x-[-1]' />
                 </div>
 
-                <div className='w-[10vw] h-[1vh] bg-[#2a2a2a] rounded-full'>
+                <div className='w-full h-[1vh] bg-[#2a2a2a] rounded-full'>
                     <div className='bg-white w-0 h-full rounded-full animate-bar'>
                     </div>
                 </div>
