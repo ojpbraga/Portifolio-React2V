c67725d8abe2dee0bc39b904072b37d5ad234a6d
João Pedro O. Braga <ojpbraga@gmail.com>
2025-01-22 10:54:39 -0300

    feat: desktop page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Loader from './components/Loader/Loader';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
+import Desktop from './components/Desktop/Desktop';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path='/' element={<Loader/>}/>
         <Route path='Home' element={<Home/>}/>
         <Route path='Login' element={<Login/>}/>
+        <Route path='Desktop' element={<Desktop/>}/>
       </Routes>
     </BrowserRouter>
   )
---
+++ src/components/Desktop/Desktop.jsx
@@ -0,0 +1,15 @@
+import Wallpaper from '/public/images/SequoiaDark.png';
+
+const Desktop = () => {
+    return (
+        <>
+       
+            <img src={Wallpaper} className='absolute h-[100dvh] w-full z-[-1]' alt="" />
+        <main>
+
+            <div className='bg-transparent'>OII</div>
+        </main>
+        </>
+    )
+}
+export default Desktop;
