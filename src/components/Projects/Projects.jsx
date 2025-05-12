473c5b3b9e373bdbf11560b9948543e2fd0ab416
João Pedro O. Braga <ojpbraga@gmail.com>
2025-05-12 19:32:16 -0300

    feat: projects pictures

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -2,45 +2,50 @@ import Slides from "./Slide";
 
 const slidesObj = [
     {
-      bgColor:"#30193E",
-      imgNotebook: '../images/mouse-cursor.png',
+      bgColor:"#0A141B",
+      imgNotebook: '../images/tsa_image.svg',
       imgLogo: '../images/mouse.png',
-      title: "Mouse Cursor", 
-      description: "Se for um site específico que foi lançado após recomendaria procurar mecanismo de busca ou em comunidades.", 
+      title: "TSA Engenharia",
+      isTitleHighlight: true,
+      titleHighlight: '#000',
+      titleHighlightWord: ['palavra'],
+      description: "Se for um site específico que foi lançado após recomendaria procurar mecanismo de busca ou em comunidades.",
+      descriptionHighlight: '#',
+      descriptionHighlightWords: [''],
       githubLink: "www.github.com", 
       figmaLink: "www.figma.com", 
       siteLink: "www.teste.com",
     },
-    {
-      bgColor:"#0ad978",
-      imgNotebook: '../images/mouse-cursor.png',
-      imgLogo: '../images/mouse.png',
-      title: "Mouse Cursor", 
-      description: "Se for um site específico que foi lançado após recomendaria procurar mecanismo de busca ou em comunidades.", 
-      githubLink: "www.github.com", 
-      figmaLink: "www.figma.com", 
-      siteLink: "www.teste.com",
-    },
-    {
-      bgColor:"#950262",
-      imgNotebook: '../images/mouse-cursor.png',
-      imgLogo: '../images/mouse.png',
-      title: "Mouse Cursor", 
-      description: "Se for um site específico que foi lançado após recomendaria procurar mecanismo de busca ou em comunidades.", 
-      githubLink: "www.github.com", 
-      figmaLink: "www.figma.com", 
-      siteLink: "www.teste.com",
-    },
-    {
-      bgColor:"#f6ff71",
-      imgNotebook: '../images/mouse-cursor.png', 
-      imgLogo: '../images/mouse.png',
-      title: "Mouse Cursor", 
-      description: "Se for um site específico que foi lançado após recomendaria procurar mecanismo de busca ou em comunidades.", 
-      githubLink: "www.github.com", 
-      figmaLink: "www.figma.com", 
-      siteLink: "www.teste.com",
-    },
+    // {
+    //   bgColor:"#fff",
+    //   imgNotebook: '../images/roadmap_image.svg',
+    //   imgLogo: '../images/mouse.png',
+    //   title: "Roadmap", 
+    //   description: "Se for um site específico que foi lançado após recomendaria procurar mecanismo de busca ou em comunidades.", 
+    //   githubLink: "www.github.com", 
+    //   figmaLink: "www.figma.com", 
+    //   siteLink: "www.teste.com",
+    // },
+    // {
+    //   bgColor:"#fff",
+    //   imgNotebook: '../images/paciente_critico_image.svg',
+    //   imgLogo: '../images/mouse.png',
+    //   title: "Paciente Crítico", 
+    //   description: "Se for um site específico que foi lançado após recomendaria procurar mecanismo de busca ou em comunidades.", 
+    //   githubLink: "www.github.com", 
+    //   figmaLink: "www.figma.com", 
+    //   siteLink: "www.teste.com",
+    // },
+    // {
+    //   bgColor:"#D2E6DD",
+    //   imgNotebook: '../images/unisus_image.svg', 
+    //   imgLogo: '../images/mouse.png',
+    //   title: "UniSUS", 
+    //   description: "Se for um site específico que foi lançado após recomendaria procurar mecanismo de busca ou em comunidades.", 
+    //   githubLink: "www.github.com", 
+    //   figmaLink: "www.figma.com", 
+    //   siteLink: "www.teste.com",
+    // },
 ]
 
 const Projects = () => {
@@ -50,7 +55,6 @@ const Projects = () => {
             <h1 className='text-[18vw] md:text-[16vw] font-bold xl:text-[6vw]'>Projetos</h1>
             <div className='w-full h-[80%]  xl:w-[60%] xl:h-[70%] max-h-[600px] '>
             <Slides slides={slidesObj}/>
-
             </div>
       </section>
     )
